3356a0c5f91205e9ff6b6e69c15c2e0d2a1a829f
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-03-26 12:58:49 -0700

    Added the first print of the project

---
+++ 0-printf.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+  * _printf - returns the number characters printed
+  * @format: string to be printed
+  * Return: number of characters
+  */
+int _printf(const char *format, ...)
+{
+	int num_of_characters = 0;
+	va_list arg_container;
+	char *str;
+	char c;
+
+	va_start(arg_container, format);
+	while (*format != '\0' && format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case '%':
+					_putchar(*format);
+					num_of_characters++;
+					break;
+				case 'c':
+					c = va_arg(arg_container, int);
+					_putchar(c);
+					num_of_characters++;
+					break;
+				case 's':
+					str = va_arg(arg_container, char *);
+					while (str && *str)
+					{
+						_putchar(*str);
+						num_of_characters++;
+						str++;
+					}
+					break;
+				default:
+					_putchar('%');
+					_putchar(*format);
+					num_of_characters+= 2;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			num_of_characters++;
+		}
+		format++;
+	}
+	va_end(arg_container);
+	return (num_of_characters);
+}
