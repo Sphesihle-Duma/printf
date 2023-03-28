3a36240f0a6e2e1a54ad6d3bc6a33de1837ec6d1
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-03-28 07:42:48 -0700

    fix

--- _printf.c
+++ _printf.c
@@ -1,115 +1,130 @@
-#include "main.h"
 #include <stdarg.h>
-#include <stdlib.h>
+#include "main.h"
 /**
- * print_char - Prints a single character to stdout
- * @c: Character to print to stdout
- * Return: Number of characters printed
+ * print_char - prints a character
+ * @valist: argument list
+ *
+ * Return: number of characters printed
  */
-int print_char(char c)
+int print_char(va_list valist)
 {
+	char c = va_arg(valist, int);
+
 	_putchar(c);
 	return (1);
 }
 /**
- * print_string - Prints a string of characters to stdout
- * @str: string to print
- * Return: Number of characters printed
+ * print_string - prints a string
+ * @valist: argument list
+ *
+ * Return: number of characters printed
  */
-int print_string(char *str)
+int print_string(va_list valist)
 {
-	int i = 0;
+	char *str = va_arg(valist, char *);
+	int len = 0;
 
-	while (str[i])
+	while (str[len])
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(str[len]);
+		len++;
 	}
-	return (i);
+	return (len);
 }
 /**
- * print_int - Prints an integer to stdout
- * @num: Integer to print
- * Return: Number of characters printed
+ * print_percent - prints a percent sign
+ * @valist: argument list
+ *
+ * Return: number of characters printed
  */
-int print_int(int num)
+int print_percent(va_list valist)
 {
-	int digit, num_of_chars = 0;
-
-	if (num == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-	if (num < 0)
-	{
-		num_of_chars += print_char('-');
-		num *= -1;
-	}
-	if (num / 10)
-		num_of_chars += print_int(num / 10);
-	digit = num % 10 + '0';
-	num_of_chars += print_char(digit);
-	return (num_of_chars);
-}
-/**
- * print_percent - Prints a single percent sign to stdout
- * Return: Number of characters printed
- */
-int print_percent(void)
-{
+	(void)valist;
 	_putchar('%');
 	return (1);
 }
 /**
- * _printf - Prints a formatted string of characters to stdout
- * @format: String to print
- * Return: Number of characters printed
+ * print_int - prints an integer
+ * @valist: argument list
+ *
+ * Return: number of characters printed
+ */
+int print_int(va_list valist)
+{
+	int n = va_arg(valist, int), div = 1, len = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		len++;
+		n *= -1;
+	}
+
+	while (n / div > 9)
+		div *= 10;
+
+	while (div != 0)
+	{
+		_putchar((n / div) % 10 + '0');
+		div /= 10;
+		len++;
+	}
+	return (len);
+}
+/**
+ * _printf - produces output according to a format
+ * @format: format string
+ *
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int num_of_chars = 0;
-	va_list arg_container;
-	char *str;
+	va_list valist;
+	int i = 0, len = 0;
 
-	va_start(arg_container, format);
-	if (!format || (format[0] == '%' && !format[1]))
+	va_start(valist, format);
+
+	if (!format)
 		return (-1);
-	while (*format)
+
+	while (format[i])
 	{
-		if (*format == '%')
+		if (format[i] == '%')
 		{
-			format++;
-			switch (*format)
+			i++;
+
+			switch (format[i])
 			{
-				case '%':
-					num_of_chars += print_percent();
-					break;
-				case 'c':
-					num_of_chars += print_char(va_arg(arg_container, int));
-					break;
-				case 's':
-					str = va_arg(arg_container, char *);
-					if (str)
-						num_of_chars += print_string(str);
-					break;
-				case 'd':
-				case 'i':
-					num_of_chars += print_int(va_arg(arg_container, int));
-					break;
-				default:
-					num_of_chars += print_percent();
-					num_of_chars += print_char(*format);
-					break;
+			case 'c':
+				len += print_char(valist);
+				break;
+			case 's':
+				len += print_string(valist);
+				break;
+			case '%':
+				len += print_percent(valist);
+				break;
+			case 'd':
+			case 'i':
+				len += print_int(valist);
+				break;
+			default:
+				_putchar('%');
+				_putchar(format[i]);
+				len += 2;
+				break;
 			}
 		}
 		else
 		{
-			num_of_chars += print_char(*format);
+			_putchar(format[i]);
+			len++;
 		}
-		format++;
+		i++;
 	}
-	va_end(arg_container);
-	return (num_of_chars);
+
+	va_end(valist);
+
+	return (len);
 }
 
